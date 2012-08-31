49a71c5060072c2cb300f7f3c5b68c8ba7a3e9a5
Jon Trent <info@jontrent.net>
2012-08-31 03:21:10 -0700

    Added subclass of GLKitViewController, SGGViewController, and MainStoryboard.

---
+++ SimpleGLKitGame/SGGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGGViewController.h
+//  SimpleGLKitGame
+//
+//  Created by Senator on 8/31/12.
+//  Copyright (c) 2012 Senator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGGViewController : GLKViewController
+
+@end
---
+++ SimpleGLKitGame/SGGViewController.m
@@ -0,0 +1,13 @@
+//
+//  SGGViewController.m
+//  SimpleGLKitGame
+//
+//  Created by Senator on 8/31/12.
+//  Copyright (c) 2012 Senator. All rights reserved.
+//
+
+#import "SGGViewController.h"
+
+@implementation SGGViewController
+
+@end
