2eef1c3d49f37612bb33fd6c3322717e3b5008c1
Jon Trent <info@jontrent.net>
2012-09-03 14:20:15 -0700

    Updated SGGViewController to show sprite. Now renders sprite using BasicEffect.

--- SimpleGLKitGame/SGGViewController.m
+++ SimpleGLKitGame/SGGViewController.m
@@ -7,13 +7,17 @@
 //
 
 #import "SGGViewController.h"
+#import "SGGSprite.h"
 
 @interface SGGViewController()
 @property (strong, nonatomic) EAGLContext *context;
+@property (strong) GLKBaseEffect *effect;
+@property (strong) SGGSprite *player;
 @end
 
 @implementation SGGViewController
 @synthesize context = _context;
+@synthesize player = _player;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -27,13 +31,23 @@
     GLKView *view = (GLKView *)self.view;
     view.context = self.context;
     [EAGLContext setCurrentContext:self.context];
+    self.effect = [[GLKBaseEffect alloc] init];
+    
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakeOrtho(0, 480, 0, 320, -1024, 1024);
+    self.effect.transform.projectionMatrix = projectionMatrix;
+    
+    self.player = [[SGGSprite alloc] initWithFile:@"Player.png" effect:self.effect];
 }
 
 #pragma mark - GLKViewDelegate
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
-    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+    glClearColor(1, 1, 1, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    
+    [self.player render];
 }
 
 - (void)update {
